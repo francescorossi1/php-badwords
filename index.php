1b67e6c1549f35ac7db9e7ac705f49c59499fbda
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-08-29 16:48:56 +0200

    created censorship, completed

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 $paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
 Tenetur, animi saepe consequuntur, temporibus architecto molestias, quod vel pariatur natus nisi odit?';
 $paragraph_len = strlen($paragraph);
+$censored_word = $_GET['word'];
+echo $paragraph . '<br>' . $paragraph_len . '<br>';
 
-echo $paragraph . '<br>' . $paragraph_len;
+$censored_paragraph = str_replace($censored_word, '***', strtolower($paragraph));
+echo $censored_paragraph . '<br>';
+
+$censored_paragraph_len = strlen($censored_paragraph);
+echo $censored_paragraph_len;
 ?>
